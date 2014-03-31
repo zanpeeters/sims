e7d2fbb01989f83feebc417de0c9c76bcde5f5c2
zanpeeters <zan@asiaa.sinica.edu.tw>
2014-03-31 17:28:49 +0800

    Set version number.

--- sims/__init__.py
+++ sims/__init__.py
@@ -2,4 +2,4 @@ from .sims import *
 from .info import *
 from .utils import *
 
-__all__ = ['SIMSBase', 'SIMSLut', 'SIMS', 'rkeys', 'coordinates', 'format_species']
+__all__ = ['SIMSBase', 'SIMSLut', 'SIMS', 'rkeys', 'coordinates', 'format_species', '__version__']
