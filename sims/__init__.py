42809dd0d67645467e41d9de9f5df4d3431f5611
zanpeeters <zan@asiaa.sinica.edu.tw>
2014-04-11 23:37:11 +0800

    Added rkeys.py

--- sims/__init__.py
+++ sims/__init__.py
@@ -1,7 +1,8 @@
 from .sims import *
 from .info import *
 from .utils import *
+form .rkeys import *
 
-__version__ = '20140402'
+__version__ = '20140411'
 
 __all__ = ['SIMSBase', 'SIMSLut', 'SIMS', 'rkeys', 'coordinates', 'format_species', '__version__']
