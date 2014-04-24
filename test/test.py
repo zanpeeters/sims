bba09147e9884d0dd4c0d1cc6d3b6ce1a1b05be0
zanpeeters <zan@asiaa.sinica.edu.tw>
2014-04-24 09:33:22 +0800

    Test files with multiple poly/offset/champs lists.

--- test/test.py
+++ test/test.py
@@ -2,21 +2,16 @@
 # -*- encoding: utf-8 -*-
 """
 Find all sims (.im, .is, .ls) files in dirs (non-recursive) and open them.
-
 Print list of files with problem.
 """
 import sims
 import glob, os
 
-###########################
+
 # list all dirs to search
 dirs = ['.', './more']
 
-# List dict keywords to print, give keywords as path,
-# e.g. 'Image/width' to print sims.header['Image']['width']
-keywords = ['sample type', 'analysis type', 'scan type', 'size type', 'NanoSIMSHeader/detector type', 'NanoSIMSHeader/scanning type',]
 
-###########################
 
 filepattern = ['*.[il][ms]']
 files = zip(dirs, filepattern * len(dirs))
@@ -27,7 +22,7 @@ for f in files:
 
 errors = []
 for f in allfiles:
-    # read header
+    # read only header
     fh = open(f, mode='rb')
     s = sims.SIMSBase(fh)
     s.peek()
@@ -36,22 +31,6 @@ for f in allfiles:
     except:
         errors.append(f)
     fh.close()
-    
-    # convert list into dict keywords
-    results = [k.split('/') for k in keywords]
-    # kstr = 's.header' + '["{}"]' * len(klist)
-    # results += [kstr.format(*klist)]
-    print(results)
-    
-    # # eval results
-    # for r in range(len(results)):
-    #     try:
-    #         results[r] = eval(results[r])
-    #     except KeyError:
-    #         results[r] = '---'
-    # 
-    # # print results
-    # print(pr.format(os.path.basename(f), *results))
 
 if errors:
     print('Problems opening:')
