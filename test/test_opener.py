aaf0befe2d55cc576f3a1a211cf9b81e23612da1
zanpeeters <zan@asiaa.sinica.edu.tw>
2014-04-29 16:36:41 +0800

    Multiple dirs and multiple filepatterns.

--- test/test_opener.py
+++ test/test_opener.py
@@ -5,21 +5,21 @@ Find all sims (.im, .is, .ls) files in dirs (non-recursive) and open them.
 Print list of files with problem.
 """
 import sims
-import glob, os
+import glob, os, itertools
 
-
-# list all dirs to search
+# list all dirs and file extensions to search
 dirs = ['.', './more']
+filepatterns = ['*.is', '*.ls', '*.im', '*.dp']
 
 
 
-filepattern = ['*.[il][ms]']
-files = zip(dirs, filepattern * len(dirs))
+files = itertools.product(dirs, filepatterns)
 files = [os.path.join(d, f) for d, f in files]
 allfiles = []
 for f in files:
     allfiles += glob.glob(f)
 
+print('Trying to open {} files...'.format(len(allfiles)))
 errors = []
 for f in allfiles:
     s = sims.SIMSOpener(f)
