2e857beac1008eb5c809fccda0c487a35ab69990
zanpeeters <zan@asiaa.sinica.edu.tw>
2016-11-30 18:42:12 +0800

    Add more unittests and reorganize files in test, doc, and devel subdirs.

--- test/open_test.py
+++ test/open_test.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+import os
 import unittest
 import sims
-
-test_files = [
-    'first_test.im',
-    'OpenMIMS_doc_040702_06-05mos-03a.im',
-    'OpenMIMS_doc_051117_05-32dRe-03b.im',
-    'OR1d6m_15.im.bz2',
-    'OR1d6m_15corr.im.bz2',  # Limage output
-]
 
 test_compression_files = [
     'first_test.im',
@@ -20,13 +13,36 @@ test_compression_files = [
     'first_test.tar.bz2'
 ]
 
+test_open_im_files = [
+    'first_test.im',
+    'OpenMIMS_doc_040702_06-05mos-03a.im',
+    'OpenMIMS_doc_051117_05-32dRe-03b.im',
+    'OR1d6m_15.im.bz2',
+    'OR1d6m_15corr.im.bz2',  # Limage output
+]
+
+test_open_other_files = [
+	'depth profile.dp',
+	'grain mode cameca.im',
+	'grain mode ciw.im',
+	'image sample scan 1.ls',
+	'line scan beam control.im',
+	'line scan stage control 1.ls',
+	'line scan stage control 2.ls',
+	'line scan stage control 3.ls',
+	'oto_std1_chain2_10.is',
+	'oxy1618_FC.is',
+	'sample stage image.ls'
+]
+
 class TestAllInput(unittest.TestCase):
     """ Unittest for all input files in test dir. """
-    
     def test_compression(self):
         """ Test that opening of compressed files works. """
-        uncompressed = sims.SIMS(test_compression_files[0])
+        uncf = os.path.join('test', test_compression_files[0])
+        unc = sims.SIMS(uncf)
         for f in test_compression_files[1:]:
+            f = os.path.join('test', f)
             with self.subTest(f=f):
                 with sims.SIMS(f) as compressed:
                     self.assertEqual(uncompressed.header, compressed.header)
@@ -39,7 +55,16 @@ class TestAllInput(unittest.TestCase):
 
     def test_open_im(self):
         """ Test opening of different flavours of the .im file. """
-        for f in test_files:
+        for f in test_open_im_files:
+            f = os.path.join('test', f)
+            with self.subTest(f=f):
+                with sims.SIMS(f) as s:
+                    pass
+
+    def test_open_other(self):
+        """ Test opening of other file types. """
+        for f in test_open_other_files:
+            f = os.path.join('test', f)
             with self.subTest(f=f):
                 with sims.SIMS(f) as s:
                     pass
