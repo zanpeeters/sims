2cd41ee57904786761e889a3148534f98cfe9fe9
zanpeeters <zan@lavabit.com>
2019-09-04 11:18:20 -0700

    Add pytest to setup.py, stop using exec.

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python
 """ Setuptools setup file for sims. """
 from setuptools import setup, find_packages
-import os
+from sims.__init__ import __version__, __name__, __author__, \
+                          __url__, __license__, __copyright__, \
+                          __description__
 
-with open(os.path.join('sims', '__init__.py'), mode='rt', encoding='utf-8') as fh:
-    script = []
-    for l in fh.readlines():
-        l = l.strip()
-        # don't import anything, just get metadata
-        if l.startswith('from') or l.startswith('import'):
-            continue
-        script.append(l)
-
-exec('\n'.join(script))
-
-with open('README.rst', mode='rt', encoding='utf-8') as fh:
+with open('README.md', mode='rt', encoding='utf-8') as fh:
     __long_description__ = fh.read()
 
+# noqa: E251
 setup(
     name = __name__,
     version = __version__,
@@ -26,9 +18,9 @@ setup(
     author = __author__,
     author_email = 'me@example.com',
     license = __license__,
+    copyright = __copyright__,
     classifiers = [
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Development Status :: 4 - Beta',
         'Development Status :: 5 - Production/Stable',
@@ -46,6 +38,8 @@ setup(
 
     packages = find_packages(),
     package_data = {'sims': ['lut/*']},
-    test_suite = 'test',
+
+    tests_require = ['pytest'],
+
     zip_safe = False
 )
